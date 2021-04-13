cdacc0e176ad01e8647073a5a1ec9985d2b0ebed
ugesh <ugesh.roopchand@hotmail.com>
2021-04-13 18:54:25 +0400

    Add active on nav-menu with onClick closeMobileMneu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,6 +6,7 @@ function Navbar() {
     // set state for click
     const [click, setClick] = useState(false);
     const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
 
     return (
         <>
@@ -17,6 +18,30 @@ function Navbar() {
                 <div className = "menu-icon" onClick={handleClick}>
                     <i className= {click ? 'fas fa-times' : 'fas fa-bars'} />
                 </div>
+                <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                    <li className="nav-item">
+                        <Link to='/' className="nav-links" onClick={closeMobileMenu}>
+                         Home
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to='/Services' className="nav-links" onClick={closeMobileMenu}>
+                         Services
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to='/Products' className="nav-links" onClick={closeMobileMenu}>
+                         Products
+                        </Link>
+                    </li>
+
+                    <li className="nav-item">
+                        <Link to='/sign-up' className="nav-links-mobile" onClick={closeMobileMenu}>
+                         Sign up
+                        </Link>
+                    </li>
+                    
+                </ul>
            </div>
           </nav>  
         </>
